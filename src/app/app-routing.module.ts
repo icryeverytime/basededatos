2c861b2add1e5a83077341a4c77a1d41e098df8e
luisvelaz <luis.velazquez@linkthinks.com>
2021-12-10 21:53:12 -0600

    Menu y componentes creados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InventarioComponent } from './components/inventario/inventario.component';
+import { ProveedoresComponent } from './proveedores/proveedores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inventario', component: InventarioComponent},
+  {path: 'proveedores', component: ProveedoresComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventarioComponent } from './components/inventario/inventario.component';
+import { ProveedoresComponent } from './proveedores/proveedores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventarioComponent,
+    ProveedoresComponent
   ],
   imports: [
     BrowserModule,
