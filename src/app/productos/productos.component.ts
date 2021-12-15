d5769ae2b1e531f3e97590b337f20f68cd42fdcf
icryeverytime <al242348@edu.uaa.mx>
2021-12-14 19:15:54 -0800

    Productos

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./productos.component.css']
 })
 export class ProductosComponent implements OnInit {
-
-  constructor() { }
+  url2='http://25.83.103.75:5000/getinventario'
+  url1='http://25.83.103.75:5000/ingredientes'
+  data1:any
+  data2:any
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-  }
 
+    this.http.post(this.url1,this.data1,{responseType: 'json'}).subscribe((result)=>{
+      console.log("inventario")
+      console.log(result)  
+      this.data1=result
+    })
+    this.http.post(this.url2,this.data2,{responseType: 'json'}).subscribe((result)=>{
+      console.log(result)  
+      this.data2=result
+      console.log("ingredientes")
+    })
+  }
 }
